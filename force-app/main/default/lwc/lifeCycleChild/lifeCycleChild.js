bced6b4980b23cebb26cb7cac7f517ec2feb3e28
Leonardo Brito <leonardo.bds@gmail.com>
2022-07-29 15:17:59 -0300

    50. Lifecycle Hooks in Unmounting Phase

---
+++ force-app/main/default/lwc/lifeCycleChild/lifeCycleChild.js
@@ -0,0 +1,16 @@
+import { LightningElement } from 'lwc';
+
+export default class LifeCycleChild extends LightningElement {
+    constructor(){
+        super();
+        console.log('Child contructor called');
+    }
+
+    connectedCallback(){
+        console.log('Child connectedCallback called');
+    }
+
+    renderedCallback(){
+        console.log('Child renderedCallback called');        
+    }
+}
---
+++ force-app/main/default/lwc/lifeCycleParent/lifeCycleParent.js
@@ -0,0 +1,21 @@
+import { LightningElement } from 'lwc';
+
+export default class LifeCycleParent extends LightningElement {
+    constructor(){
+        super();
+        console.log('parent contructor called');
+    }
+
+    connectedCallback(){
+        console.log('parent connectedCallback called');
+    }
+
+    renderedCallback(){
+        console.log('parent renderedCallback called');
+    }
+
+    name;
+    changeHandler(event){
+        this.name = event.target.value;
+    }
+}
