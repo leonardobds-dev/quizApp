e1c52db72f4093acd861536f9c2e0f7578e269fe
Leonardo Brito <leonardo.bds@gmail.com>
2022-08-02 19:23:22 -0300

    63. Child to Parent Communication Using Event Bubbling

--- force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
+++ force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
@@ -3,10 +3,15 @@ import { LightningElement } from 'lwc';
 export default class C2pModalComponent extends LightningElement {
     closeHandler(){ 
         const myEvent = new CustomEvent('close', {
+            bubbles: true,
             detail: {
                 msg:  'Modal Closed Successfuly!!!'
             }
         });
         this.dispatchEvent(myEvent);
+    }
+
+    footerHandler(){
+        console.log('footerHandler called');
     }
 }
