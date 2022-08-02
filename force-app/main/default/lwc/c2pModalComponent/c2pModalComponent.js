c67389caf89902e37ada40f88e90ef6295c870de
Leonardo Brito <leonardo.bds@gmail.com>
2022-08-02 18:47:04 -0300

    62. Child to Parent Communication Using Event with Data

--- force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
+++ force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
@@ -2,7 +2,11 @@ import { LightningElement } from 'lwc';
 
 export default class C2pModalComponent extends LightningElement {
     closeHandler(){ 
-        const myEvent = new CustomEvent('close');
+        const myEvent = new CustomEvent('close', {
+            detail: {
+                msg:  'Modal Closed Successfuly!!!'
+            }
+        });
         this.dispatchEvent(myEvent);
     }
 }
