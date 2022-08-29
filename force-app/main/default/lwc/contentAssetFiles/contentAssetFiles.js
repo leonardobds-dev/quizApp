d40ed36ca92be21c366c8c1bab3046ee3035d8ce
Leonardo Brito <leonardo.bds@gmail.com>
2022-08-29 19:49:33 -0300

    updated

--- force-app/main/default/lwc/contentAssetFiles/contentAssetFiles.js
+++ force-app/main/default/lwc/contentAssetFiles/contentAssetFiles.js
@@ -1,7 +1,9 @@
 import { LightningElement } from 'lwc';
-import TYPESCRIPT_FILE from '@salesforce/contentAssetUrl/CVLeonardoBritopdf';
+import PERSONAL_CV from '@salesforce/contentAssetUrl/CVLeonardoBritopdf';
+import TYPESCRIPT_FILE from '@salesforce/contentAssetUrl/typescripthandbookbetapdf';
 
 export default class ContentAssetFiles extends LightningElement {
     file = TYPESCRIPT_FILE;
+    personalCv = PERSONAL_CV;
     //file = '';
 }
