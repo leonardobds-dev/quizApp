bee4f692b73c61a5855157380efbace8b5703b2d
Leonardo Brito <leonardo.bds@gmail.com>
2022-08-29 17:56:44 -0300

    78. Content Asset Files

---
+++ force-app/main/default/lwc/contentAssetFiles/contentAssetFiles.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+import TYPESCRIPT_FILE from '@salesforce/contentAssetUrl/CVLeonardoBritopdf';
+
+export default class ContentAssetFiles extends LightningElement {
+    file = TYPESCRIPT_FILE;
+    //file = '';
+}
