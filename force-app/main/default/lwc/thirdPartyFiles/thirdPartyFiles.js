a4b593018d04aeadf1f84fb2aeec9cf9d60959f5
Leonardo Brito <leonardo.bds@gmail.com>
2022-08-29 16:51:44 -0300

    76. Third Party JavaScript Libraries in LWC

---
+++ force-app/main/default/lwc/thirdPartyFiles/thirdPartyFiles.js
@@ -0,0 +1,28 @@
+import { LightningElement } from 'lwc';
+import MOMENT from '@salesforce/resourceUrl/moment';
+import { loadScript } from 'lightning/platformResourceLoader';
+
+export default class ThirdPartyFiles extends LightningElement {
+    
+    currentDate = '';
+    isLibLoaded = false;
+    
+    renderedCallback(){
+        if(this.isLibLoaded){
+            return;
+        }else{
+            loadScript(this, MOMENT + '/moment/moment.min.js').then(()=>{
+            //success
+            this.setDateOnScreen();
+            }).catch(error=>{
+                console.error(error);
+            })
+            this.isLibLoaded = true;
+        }
+    }
+
+    setDateOnScreen(){
+        this.currentDate = moment().format('LLLL');
+    }
+
+}
