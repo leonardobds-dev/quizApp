4b3f1c8f21761fd98400a472df9be70ae7e027a3
Leonardo Brito <leonardo.bds@gmail.com>
2022-09-06 12:39:27 -0300

    113. @wire service and fetch user details

---
+++ force-app/main/default/lwc/wireUserDetailDemo/wireUserDetailDemo.js
@@ -0,0 +1,25 @@
+import { LightningElement, wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+import Id from '@salesforce/user/Id';
+import NAME_FIELD from '@salesforce/schema/User.Name';
+import EMAIL_FIELD from '@salesforce/schema/User.Email';
+const fields = [NAME_FIELD, EMAIL_FIELD];
+
+export default class WireUserDetailDemo extends LightningElement {
+    userId = Id; //'0058a00000LW1weAAD'
+    userDetailFunction;
+
+    //@wire(getRecord, {recordId: Id, fields: [NAME_FIELD, EMAIL_FIELD]})
+    @wire(getRecord, {recordId: Id, fields})
+    userDetailHandler({data, error}){
+        if(data){
+            this.userDetailFunction = data.fields;
+        }
+        if(error){
+            console.log(error);
+        }
+    }
+
+    @wire(getRecord, {recordId: Id, fields: [NAME_FIELD, EMAIL_FIELD]})
+    userDetailProperty;
+}
