a0d46233d284df6bb995c5cdc8d297f02be525d9
Leonardo Brito <leonardo.bds@gmail.com>
2022-09-06 16:02:07 -0300

    116. getObjectInfos adapter

--- force-app/main/default/lwc/getObjectInfoDemo/getObjectInfoDemo.js
+++ force-app/main/default/lwc/getObjectInfoDemo/getObjectInfoDemo.js
@@ -1,22 +1,21 @@
 import { LightningElement, wire } from 'lwc';
-import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import {getObjectInfo, getObjectInfos} from 'lightning/uiObjectInfoApi';
 import ACCOUNT_OBJECT from '@salesforce/schema/Account';
+import OPPORTUNITY_OBJECT from '@salesforce/schema/Opportunity';
 
 export default class GetObjectInfoDemo extends LightningElement {
 
-    defaultRecordTypeId_Field;
-
-    @wire(getObjectInfo, { objectApiName: ACCOUNT_OBJECT })
+    @wire(getObjectInfo, {objectApiName:ACCOUNT_OBJECT})
     objectInfo;
 
-    // @wire(getObjectInfo, { objectApiName: ACCOUNT_OBJECT })
-    // objectInfo({data, error}){
-    //     if(data){
-    //         console.log(data);
-    //         this.defaultRecordTypeId_Field = data.defaultRecordTypeId;
-    //     }
-    //     if(error){
-    //         console.log(error);
-    //     }
-    // }
+    objectApiNames = [ACCOUNT_OBJECT, OPPORTUNITY_OBJECT];
+
+    objectInfos;
+    @wire(getObjectInfos, { objectApiNames: '$objectApiNames' })
+    objectInfosHandler({data}){
+        if(data){
+            console.log(data);
+            this.objectInfos = data;
+        }
+    }
 }
