0d6c9efb94ae74518f5395e8ba44abb9fec49853
Leonardo Brito <leonardo.bds@gmail.com>
2022-09-08 22:32:53 -0300

    135. Meta Configuration in LWC

---
+++ force-app/main/default/lwc/lwcConfigDemo/lwcConfigDemo.js
@@ -0,0 +1,10 @@
+import { LightningElement, api } from 'lwc';
+
+export default class LwcConfigDemo extends LightningElement {
+    
+    @api heading;
+    @api recordId;
+    @api age;
+    @api levels;
+
+}
